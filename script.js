597c3162c6fda0c86caaad9e8c4f8bc4121f2897
Cody Bouscaren <codybousc@gmail.com>
2016-04-05 14:44:58 -0400

    Exercises Skeleton

---
+++ script.js
@@ -0,0 +1,30 @@
+
+/*
+
+      1) Create an array of numbers 1 - 6.
+Without using loops, console log every even number in the array. (Hint: there will be multiple console.log statements)
+
+
+      2) Create an array of numbers 1 -10
+Using a for loop, loop over this array and console.log every number in the array
+Now change the condition so that the loop only console logs numbers that are greater than 5.
+Using a for loop, loop over the array and console.log all even numbers. Hint: you need an if condition inside the body of the loop. Also look into the use of Modulo (%)
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Arithmetic_Operators
+
+
+      3) Create an array of named after either a favorite superhero, favorite band or favorite sports team. List, as strings, at least six elements in the array with information relevant to the array. For example, if your array represents a superhero you might want an array of his/her enemies. If your array represents your favorite band, you may want an array of the locations of several upcoming concerts.
+Use push to add another element to the end of the array.
+Change one specific element in the array using the arrayName[2] = “newString” syntax
+Remove the last element of the array using the .pop() method
+Loop over the array and print out all elements using a for loop
+Loop over the array and print out all elements using a while loop
+
+4) Create a basic HTML page and an array of colors.
+Add JQuery to the page and use it to alter the background color of the body.
+Create two buttons that when clicked will update the body of the page with their respective colors.
+Create a button that when clicked will loop over every color in the array altering the background color with each loop
+
+*/
+
+//Answers go below this line
+//===================================================================================
